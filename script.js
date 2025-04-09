c4949fe8b1be282cfdc9308eaeb983a56761b00d
Jorge Renato <jorgerenato032@gmail.com>
2025-04-09 18:27:20 -0400

    ..

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function createHeart() {
   const randomX = Math.random() * window.innerWidth;
   heart.style.left = `${randomX}px`;
 
-  // Adiciona dentro da div container
+  
   const container = document.querySelector('.coracoes-container');
   container.appendChild(heart);
 
